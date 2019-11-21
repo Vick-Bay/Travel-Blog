96b097f9cb099365ef8907ed2bd4283d3e24bc2f
Vick-Bay <levicking@hotmail.com>
2019-11-21 23:06:21 +0400

    v9

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -25,7 +25,11 @@ router.post("/", isLoggedIn, function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description;
-	var newCampground = {name: name, image: image, description: desc};
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+	var newCampground = {name: name, image: image, description: desc, author: author};
 	
 	//Create a new campground and save to DB
 	
@@ -34,6 +38,7 @@ router.post("/", isLoggedIn, function(req, res){
 			console.log(err);
 		} else {
 			//redirect back to campgrounds page
+			console.log(newlyCreated);
 			res.redirect("/campgrounds");
 		}
 	});	
