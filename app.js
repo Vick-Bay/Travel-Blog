d5df8ef4c1391b12ba129e87492b6fbb8486ad77
Vick-Bay <50940714+Vick-Bay@users.noreply.github.com>
2019-12-03 10:16:50 +0400

    adding flash

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var flash = require("connect-flash");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
 var methodOverride = require("method-override");
@@ -26,6 +27,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(path.resolve('./public')));
 app.use(methodOverride("_method"));
+app.use(flash());
 
 
 //seedDB(); //seed the database
