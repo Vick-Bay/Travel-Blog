30293d2bcd0a401ce4d1feb9590b0786f081a544
Vick-Bay <50940714+Vick-Bay@users.noreply.github.com>
2019-12-06 13:01:39 +0400

    v11

--- routes/index.js
+++ routes/index.js
@@ -23,10 +23,11 @@ router.post("/register", function(req, res){
 	var newUser = new User({username: req.body.username});
 	User.register(newUser, req.body.password, function(err, user){
 		if(err){
-			console.log(err);
-			return res.render("register");
+			req.flash("error", err.message);
+			return res.redirect("/register");
 		}
 		passport.authenticate("local")(req, res, function(){
+			req.flash("success", "Welcome to YelpCamp" + user.username);
 			res.redirect("/campgrounds");
 		});
 	});
@@ -34,7 +35,7 @@ router.post("/register", function(req, res){
 
 //show login form
 router.get("/login", function(req, res){
-	res.render("login",{message: req.flash("error")});
+	res.render("login");
 });
 
 //handling login logic
@@ -49,14 +50,10 @@ router.post("/login", passport.authenticate("local",
 
 router.get("/logout", function(req, res){
 	req.logout();
+	req.flash("success", "Logged you out!");
 	res.redirect("/campgrounds");
 });
 
-//middleware
-function isLoggedIn(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	}
-	res.redirect("/login");
-}
+
+
 module.exports = router;
